c467ecda0d99dab5859f6ff443ebd72e7912a4f3
Amraj Aulak <108581972+AmrajAulak@users.noreply.github.com>
2023-05-01 11:53:16 +0100

    Hole changes height

--- script.js
+++ script.js
@@ -4,25 +4,36 @@
 //     document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
 // }p
 
-function run_game(){
-    //let bird = document.querySelector("bird");
+// function run_game(){
+//     //let bird = document.querySelector("bird");
+
+// }
+
+// let bird = document.getElementById("bird");
+// let score = document.getElementById("score");
+
+// let x = bird.offsetLeft;
+// let y = bird.offsetTop;
+// let bWidth = bird.offsetWidth;
+
+// console.log(bWidth)
+
+// console.log("Element X: " + x);
+// console.log("Element Y: " + y);
+
+// bird.style.left = 0
+// bird.style.top = 0
+
+const pipe = document.getElementById("pipe");
+// pipe.addEventListener("animationend", myEndFunction, false);
+
+function changeHeights(){
+    console.log("end");
+    const hole = document.getElementById("hole");
+    let y = Math.floor(Math.random() * 250);
+    hole.style.top = y + "px";
 
 }
 
-let bird = document.getElementById("bird");
-let score = document.getElementById("score");
 
-let x = bird.offsetLeft;
-let y = bird.offsetTop;
-let bWidth = bird.offsetWidth;
-
-console.log(bWidth)
-
-console.log("Element X: " + x);
-console.log("Element Y: " + y);
-
-bird.style.left = 0
-bird.style.top = 0
-
-
-
+pipe.addEventListener("animationiteration", changeHeights);
