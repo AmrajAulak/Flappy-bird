7c96aa91199efab4c6418f854ae89834d9be228d
Amraj Aulak <108581972+AmrajAulak@users.noreply.github.com>
2023-05-01 14:49:13 +0100

    collision detection

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
-// window.onclick = myFunction;
 
-// function myFunction() {
-//     document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
-// }p
-
-// function run_game(){
-//     //let bird = document.querySelector("bird");
-
-// }
-
-// let bird = document.getElementById("bird");
-// let score = document.getElementById("score");
-
-// let x = bird.offsetLeft;
-// let y = bird.offsetTop;
-// let bWidth = bird.offsetWidth;
-
-// console.log(bWidth)
-
-// console.log("Element X: " + x);
-// console.log("Element Y: " + y);
-
-// bird.style.left = 0
-// bird.style.top = 0
-
-const pipe = document.getElementById("pipe");
-// pipe.addEventListener("animationend", myEndFunction, false);
+var scoreValue = 0;
 
 function changeHeights(){
     console.log("end");
     const hole = document.getElementById("hole");
-    let y = Math.floor(Math.random() * 250);
+    let y = Math.floor(Math.random() * 225);
     hole.style.top = y + "px";
-
+    let score = document.querySelector(".text");
+    scoreValue ++;
+    score.innerHTML = "Score: " + scoreValue;
 }
 
+function run_game(){
+    pipe.addEventListener("animationiteration", changeHeights);
+    window.onclick = jump;
+   
+    let ID = setInterval(gameLoop,50)
+    let fall_speed = 3
+    let jump_speed = 35;
 
-pipe.addEventListener("animationiteration", changeHeights);
+    function gameLoop(){
+        gravity()
+        if (check_collision()){
+            return
+        }
+    }
+
+    function check_collision(){
+        let bird = document.getElementById("bird");
+        let hole = document.getElementById("hole");
+        let bird_top = bird.offsetTop;
+        let bird_bottom = bird_top + bird.offsetHeight;
+        let bird_right = bird.offsetLeft + bird.offsetWidth; 
+        let hole_left = hole.offsetLeft;
+        let hole_top = hole.offsetTop;
+        let hole_bottom = hole_top + hole.offsetHeight;
+
+
+        if ((bird_bottom >= 400) || 
+        ((bird_right >= hole_left) && (bird_top <= hole_top)) || 
+        ((bird_right >= hole_left) && (bird_bottom >= hole_bottom))) {
+            clearInterval(ID);
+            document.querySelector("#pipe").style.animationPlayState = 'paused';
+            hole.style.animationPlayState = 'paused';
+            bird.style.transform = 'rotate(180deg)';
+
+        }
+
+    }
+    
+    function gravity(){
+        let bird = document.getElementById("bird");
+        let bird_y = bird.offsetTop;
+        bird.style.top = bird_y + fall_speed +"px";
+    }
+
+    function jump(){
+        let bird = document.getElementById("bird");
+        let bird_y = bird.offsetTop;
+        if ((bird_y - jump_speed) >= 0){
+            bird.style.top = bird_y - jump_speed +"px";
+        }
+    }
+}
+
+run_game()
