04fb1e317db37d7cc959d74a8bda783eea6a14fd
Amraj Aulak <108581972+AmrajAulak@users.noreply.github.com>
2023-05-02 20:33:52 +0100

    add clouds in background

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 
 var scoreValue = 0;
 
-function changeHeights(){
-    console.log("end");
-    const hole = document.getElementById("hole");
-    let y = Math.floor(Math.random() * 225);
-    hole.style.top = y + "px";
+function updateScore(){
     let score = document.querySelector(".text");
     scoreValue ++;
     score.innerHTML = "Score: " + scoreValue;
 }
 
+function changeHeights(){
+    // const hole = document.getElementById("hole");
+    // let y = Math.floor(Math.random() * 225);
+    // hole.style.top = y + "px";
+    const pipe_upper = document.getElementById("pipe_top");
+    const pipe_bottom = document.getElementById("pipe_bottom");
+    const gap_size = 150;
+    let height = Math.floor(Math.random() * 225);
+    pipe_upper.style.height = height + "px";
+    pipe_bottom.style.top = height + gap_size + "px";
+    pipe_bottom.style.height = 400 - height - gap_size + "px";
+    
+    updateScore();
+}
+
 function run_game(){
-    pipe.addEventListener("animationiteration", changeHeights);
+    pipe_top.addEventListener("animationiteration", changeHeights);
     window.onclick = jump;
    
     let ID = setInterval(gameLoop,50)
@@ -27,22 +38,24 @@ function run_game(){
     }
 
     function check_collision(){
-        let bird = document.getElementById("bird");
-        let hole = document.getElementById("hole");
+        const bird = document.getElementById("bird");
+        const pipe_upper = document.getElementById("pipe_top");
+        const pipe_bottom = document.getElementById("pipe_bottom");
+
         let bird_top = bird.offsetTop;
         let bird_bottom = bird_top + bird.offsetHeight;
         let bird_right = bird.offsetLeft + bird.offsetWidth; 
-        let hole_left = hole.offsetLeft;
-        let hole_top = hole.offsetTop;
-        let hole_bottom = hole_top + hole.offsetHeight;
 
+        let pipe_left = pipe_upper.offsetLeft;
+        let hole_top = pipe_upper.offsetTop + pipe_upper.offsetHeight;
+        let hole_bottom = pipe_bottom.offsetTop;
 
         if ((bird_bottom >= 400) || 
-        ((bird_right >= hole_left) && (bird_top <= hole_top)) || 
-        ((bird_right >= hole_left) && (bird_bottom >= hole_bottom))) {
+        ((bird_right >= pipe_left) && (bird_top <= hole_top)) || 
+        ((bird_right >= pipe_left) && (bird_bottom >= hole_bottom))) {
             clearInterval(ID);
-            document.querySelector("#pipe").style.animationPlayState = 'paused';
-            hole.style.animationPlayState = 'paused';
+            document.querySelector("#pipe_top").style.animationPlayState = 'paused';
+            document.querySelector("#pipe_bottom").style.animationPlayState = 'paused';
             bird.style.transform = 'rotate(180deg)';
 
         }
